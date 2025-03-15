ce89204f8a8f5380d0b8381ed70d1ed5b3e21d29
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2025-03-15 00:21:12 +0000

    commented_in_devise_routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {  #管理者用のdeviseルーティング 今回は新規登録機能・パスワード変更機能は不要
     sessions: "admin/sessions"
   }
 
-  devise_for :customers, skip: [:passwords], controllers: {
+  devise_for :customers, skip: [:passwords], controllers: {  #顧客用のdeviseルーティング 今回はパスワード変更機能は不要
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # for details on the dsl available within this file, see https://guides.rubyonrails.org/routing.html
 end
